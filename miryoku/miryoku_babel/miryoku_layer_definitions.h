d41ebe6441dc3d2b42580b4612963388311dcbb9
Jacob Moore <jacobmoor2@gmail.com>
2025-05-18 15:10:39 -0600

    removed rgb keys from media layer (no rgbs on my kbd)

--- miryoku/miryoku_babel/miryoku_layer_definitions.h
+++ miryoku/miryoku_babel/miryoku_layer_definitions.h
@@ -52,7 +52,7 @@ U_NP,              U_NP,              U_NA,              U_NA,              U_NA
 
 
 #define MIRYOKU_ALTERNATIVES_MEDIA_VI \
-U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              U_RGB_EFF,         &kp C_BRI_UP,      &kp C_BRI_DN,      U_RGB_BRI,         U_RGB_TOG,         \
+U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              U_NU,              &kp C_BRI_DN,      &kp C_BRI_UP,      U_NU,              U_NU,              \
 &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &kp C_PREV,        &kp C_VOL_DN,      &kp C_VOL_UP,      &kp C_NEXT,        U_EP_TOG,          \
 U_NA,              &kp RALT,          &u_to_U_FUN,       &u_to_U_MEDIA,     U_NA,              &u_bt_sel_0,       &u_bt_sel_1,       &u_bt_sel_2,       &u_bt_sel_3,       &u_out_tog,        \
 U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp C_STOP,        &kp C_PP,          &kp C_MUTE,        U_NP,              U_NP
